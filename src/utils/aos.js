16e46dae2f9bbd4fd4d7ac086d61a852d9348695
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-06-01 01:52:38 +0330

    aos configs

---
+++ src/utils/aos.js
@@ -0,0 +1,33 @@
+'use client'
+import { useEffect } from "react"
+
+// AOS 
+import Aos from "aos"
+import 'aos/dist/aos.css'
+
+
+export default function AOSInit() {
+
+    useEffect(() => {
+        const aosAnimation = document.querySelectorAll('[data-aos]');
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+              entry.target.classList.add('aos-animate');
+            } else {
+              entry.target.classList.remove('aos-animate');
+            }
+          });
+        });
+        aosAnimation.forEach(aosObject => {
+          observer.observe(aosObject);
+        });
+        Aos.init({
+          duration: 600,
+          delay: 100
+        })
+    }, [])
+
+    return null
+
+}
