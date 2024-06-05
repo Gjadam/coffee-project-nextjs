cc0458f2dd292bbeee00c7bd42a71f840a9291f0
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-06-06 02:50:34 +0330

    creating loading screen

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+
+// Components
+import Loading from './components/modules/Loading/Loading'
+
+export default function loading() {
+    return (
+        <>
+        <Loading/>
+        </>
+    )
+}
