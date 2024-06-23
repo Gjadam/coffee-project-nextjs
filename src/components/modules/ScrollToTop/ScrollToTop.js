d74ae21c7e20181545efe1a66eb76ca133d7a2b2
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-06-23 23:38:32 +0330

    creating ScrollToTop

---
+++ src/components/modules/ScrollToTop/ScrollToTop.js
@@ -0,0 +1,37 @@
+'use client'
+import { useEffect, useState } from "react";
+// Icons
+import { IoIosArrowUp } from "react-icons/io";
+
+export default function ScrollToTop() {
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+
+  const [showScrollToTop, setShowScrollToTop] = useState(false)
+
+  useEffect(() => {
+      const showScrollToTop = () => {
+          const currentScroll = window.pageYOffset
+          if (currentScroll > 105) {
+            setShowScrollToTop(true)
+          } else {
+            setShowScrollToTop(false)
+          }
+      }
+
+      window.addEventListener('scroll', showScrollToTop)
+      return () => window.removeEventListener('scroll', showScrollToTop)
+  }, [])
+
+
+  return (
+    <div className={`fixed left-7 ${showScrollToTop ? ' bottom-7  visible opacity-100 ' : '-bottom-20 invisible opacity-0 '}  z-50 text-2xl p-2 rounded  bg-primary hover:bg-secondary hover:text-white transition-all duration-300 cursor-pointer `} onClick={scrollToTop}>
+      <IoIosArrowUp />
+    </div>
+  )
+}
