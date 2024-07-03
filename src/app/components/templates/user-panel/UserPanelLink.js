b85665ed0cc60e6156f8bea3977970aa796f957b
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-07-03 22:40:28 +0330

    deleted files

--- src/app/components/templates/user-panel/UserPanelLink.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function UserPanelLink({ text, children }) {
-  return (
-    <div className="  w-full text-center p-3 rounded-3xl  hover:last:bg-red-600 hover:bg-primary text-secondary hover:text-white transition-colors">
-      <span className=' flex items-center gap-3'>{children} {text}</span>
-    </div>
-  )
-}
