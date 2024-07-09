957e2331cf0afc14485abb49abe8f83aaa400c59
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-07-09 13:48:27 +0330

    Database

---
+++ configs/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const connectToDB = async () => {
+    try {
+        if(mongoose.connections[0].readyState) {
+            return true
+        } else {
+            await mongoose.connect(process.env.MONGO_URL)
+            console.log('Connect To DB Successfully');
+        }
+    } catch (err) {
+        console.log('DB connection has error ->', err);
+    }
+}
+
+
+export default connectToDB
