c6eaa3e1677e3e10cf967aeda1659a25e9b80c72
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-07-09 13:52:24 +0330

    Tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': '#be9c79',
+        'secondary': '#1c1c1c',
+      },
+      borderWidth: {
+        '1': '1px'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'toBottomFromTop': 'toBottomFromTop 0.9s linear infinite',
+      },
+      keyframes: {
+        toBottomFromTop: {
+          '49%' : {opacity: '0', transform: 'translateY(10px)' },
+          '50%' : { opacity: '0', transform: 'translateY(-100%)' },
+          '51%' : { opacity: '0' },
+        },
+      }
     },
   },
   plugins: [],
