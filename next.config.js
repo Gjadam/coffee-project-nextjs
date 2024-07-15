629ee964b7d3068d3e8ac05a5c35bfdedac3ddaa
DESKTOP-KBCTF04\Elecomp <gjadjob@gmail.com>
2024-07-15 06:49:38 +0330

    update

--- next.config.js
+++ next.config.js
@@ -5,7 +5,32 @@ const nextConfig = {
     serverComponentsExternalPackages: ["mongoose"]
   },
   images: {
-    domains: ['localhost'],
+    domains: [
+      'arweave.net',
+      'bitcorns.com',
+      'cdn.bitcoincoaster.com',
+      'cdn.sanity.io',
+      'dankdirectory.files.wordpress.com',
+      'fakeraredirectory.com',
+      'fakeraredirectory.files.wordpress.com',
+      'hosting.photobucket.com',
+      'ipfs.io',
+      'i.imgur.com',
+      'mandelduck.herokuapp.com',
+      'metadata.spellsofgenesis.com',
+      'pepewtf.s3.amazonaws.com',
+      'phunchkins.com',
+      'public-scarce-city.s3.amazonaws.com',
+      'rarejapanesenfts.com',
+      'rarepepedirectory.com',
+      'rarepepewallet.com',
+      'scannablenfts.com', // <--- here it is
+      'thewojakway.com',
+      'xchain.io',
+      'xcp.dankdirectory.site',
+      'xcp.dankinfo.art',
+      'xcpinata.s3.amazonaws.com',
+    ],
   },
 }
 
